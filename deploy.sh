3344cf94170f613ea488f9cbeb8af40ffa4dcec7
JianchaoLiu <jianchaoliu@webot.co>
2020-11-10 21:09:52 +0800

    chore: github actions

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 git branch -M main
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:JChehe/study_actions.git main:gh-pages
 
